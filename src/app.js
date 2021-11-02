f0bceb1d0b224a25e70e7e087657b7da0b4a3d25
yesireth <yesireth88@gmail.com>
2021-11-02 11:15:01 -0500

    Se agregaron funcionalidades de las ordenes

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const config = require('./config');
 
-// const authMiddleware = require('./middleware/auth');
+
 const errorHandler = require('./middleware/error');
 
 const routes = require('./routes');
